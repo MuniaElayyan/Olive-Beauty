b8661cc2a849963ac18728f0a64bf209bd3a3a3a
Munia Elayyan <MuniaElayyan2024@gmail.com>
2026-08-07 22:28:49 +0300

    login.js

--- login.js
+++ login.js
@@ -1,79 +1,31 @@
-// =================================
-// عناصر صفحة تسجيل الدخول
-// =================================
-
 let username = document.getElementById("username");
-
 let password = document.getElementById("password");
-
 let loginBtn = document.getElementById("loginBtn");
-
 let message = document.getElementById("message");
 
-
-
-// =================================
-// فحص البيانات عند الضغط على Login
-// =================================
-
 loginBtn.onclick = function(){
-
-
 if(username.value === ""){
-
 username.style.border = "2px solid red";
-
 message.innerHTML = "Please enter username";
-
 return;
-
 }
-
-
 
 if(password.value === ""){
-
 password.style.border = "2px solid red";
-
 message.innerHTML = "Please enter password";
-
 return;
-
 }
-
-
-
-// إذا كتب أي شيء يدخل للموقع
 
 localStorage.login = "true";
-
-
 window.location.href = "1.html";
-
-
 };
 
-
-
-
-// =================================
-// إزالة اللون الأحمر عند الكتابة
-// =================================
-
 username.oninput = function(){
-
 username.style.border = "1px solid #B8A06A";
-
 message.innerHTML = "";
-
 }
 
-
-
 password.oninput = function(){
-
 password.style.border = "1px solid #B8A06A";
-
 message.innerHTML = "";
-
 }
